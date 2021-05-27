43c058a21139b6437ec5c9857eec3163d914f389
LuisMiguelRG <luis.miguel.dj22@gmail.com>
2021-05-27 15:42:10 -0500

    incrementa RG

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  rconstructor() {
+    super();
+    this.state = { count: 0 }
+  };
+
+
+  count(event) {
+    this.setState({
+      count: event.target.value.length
+    });
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.count.bind(this)}></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
   }
